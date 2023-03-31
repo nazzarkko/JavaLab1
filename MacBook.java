a15214a54a0c0c787c6238782040aafac26e4c42
nazzarkko <119264236+nazzarkko@users.noreply.github.com>
2023-04-01 00:01:07 +0300

    Add files via upload

---
+++ MacBook.java
@@ -0,0 +1,31 @@
+package ua.lviv.iot.algo.part1.lab2;
+import lombok.ToString;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+
+@AllArgsConstructor
+@Getter
+@NoArgsConstructor
+@ToString
+@Setter
+
+public class MacBook extends AbstractLaptop{
+protected int capacityInHours;
+protected String modelOfProcessor;
+protected String versionOfIOS;
+
+    public MacBook(String model, int ram, int screenSizeInch, int capacityInHours, String modelOfProcessor, String versionOfIOS) {
+        super(model, ram, screenSizeInch);
+        this.capacityInHours = capacityInHours;
+        this.modelOfProcessor = modelOfProcessor;
+        this.versionOfIOS = versionOfIOS;
+    }
+
+    public String replaceBatttery(int capacityInHours) {
+this.capacityInHours=capacityInHours;
+        return "Laptop with new battery can work " +capacityInHours;
+    }
+}
