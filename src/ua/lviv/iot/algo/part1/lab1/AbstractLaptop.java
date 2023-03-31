e1e9946c849e0cfc09963ebe44a70893b86856ae
nazzarkko <119264236+nazzarkko@users.noreply.github.com>
2023-03-31 23:44:05 +0300

    LabNumber2

---
+++ src/ua/lviv/iot/algo/part1/lab1/AbstractLaptop.java
@@ -0,0 +1,27 @@
+package ua.lviv.iot.algo.part1.lab2;
+
+import lombok.ToString;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+
+@AllArgsConstructor
+@Getter
+@NoArgsConstructor
+@ToString
+@Setter
+
+
+abstract class AbstractLaptop {
+   protected String model;
+   protected int ram;
+   protected int screenSizeInch;
+   public abstract String replaceBatttery(int capacityInHours);
+
+
+
+    }
+
+
